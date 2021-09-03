96da59ef5a08e771e77a69739afb12650953e4cc
Joshua Beadle <rebelmusicprods@gmail.com>
2021-09-03 17:03:12 -0600

    forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import { createAppContainer } from 'react-navigation';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
 
     const mapDispatchToProps = {
         fetchCampsites,
@@ -112,6 +113,28 @@ const ContactNavigator = createStackNavigator(
                 },
                 headerLeft: <Icon
                     name='info-circle'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            })
+        }
+    );
+    const ReservationNavigator = createStackNavigator(
+        {
+            Reservation: { screen: Reservation }
+        },
+        {
+            defaultNavigationOptions: ({navigation}) => ({
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    color: '#fff'
+                },
+                headerLeft: <Icon
+                    name='tree'
                     type='font-awesome'
                     iconStyle={styles.stackIcon}
                     onPress={() => navigation.toggleDrawer()}
